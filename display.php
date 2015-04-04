f729e8783fbfafeb64f9eafb2e765d8ff4b87f63
PunKeel <punkeel@me.com>
2015-04-04 16:15:58 +0200

    Fix bugs!

--- display.php
+++ display.php
@@ -16,11 +16,25 @@ function isAnimatedGif($filename) {
     return (bool)preg_match('#(\x00\x21\xF9\x04.{4}\x00\x2C.*){2,}#s', file_get_contents($filename));
 }
 
-$filename = pathinfo(parse_url($_SERVER['REQUEST_URI'])['path'])['filename'];
-$files = glob(BASE_PATH . $filename . '.*');
-if(empty($files)){
-	header('HTTP/1.0 404 Not Found', true, 404);
-	die('404.');
+function send_image($path, $is_image = false, $mime = ''){
+	header('Content-Description: File Transfer');
+	header('Content-Type: ' . $mime);
+	header('Content-Disposition: ' . ($is_image?'inline':'attachement') . '; filename='.basename($path));
+	header('Content-Length: ' . filesize($path));
+	readfile($path);
+	exit;
+}
+
+$filename = BASE_PATH . pathinfo(parse_url($_SERVER['REQUEST_URI'])['path'])['basename'];
+
+$files = array($filename);
+
+if(!file_exists($filename)){
+	$files = glob($filename . '.*');
+	if(empty($files)){
+		header('HTTP/1.0 404 Not Found', true, 404);
+		die('404.');
+	}
 }
 
 $f = $files[0];
@@ -28,25 +42,30 @@ $finfo = finfo_open(FILEINFO_MIME_TYPE); // Retourne le type mime à la extensio
 $mime = finfo_file($finfo, $f);
 finfo_close($finfo);
 $is_image = substr($mime, 0, 6) == 'image/';
-if (stripos($_SERVER['HTTP_USER_AGENT'], 'Twitterbot') !== false) {
-	header('Vary: User-Agent');
+
+if (stripos($_SERVER['HTTP_USER_AGENT'], 'Twitterbot') === false || basename($filename) == basename($f)) {
+	send_image($f, $is_image, $mime);
+}
+
+// Twitter integration
+header('Vary: User-Agent');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<?php if($is_image){ ?>
-	<?php if(isAnimatedGif($f)){ ?>
-		<?php list($width, $height) = getimagesize($f); ?>
+	<?php if($is_image): ?>
+		<?php if(isAnimatedGif($f)): ?>
+			<?php list($width, $height) = getimagesize($f); ?>
 		<meta name="twitter:card" content="player" />
 		<meta name="twitter:player:width" content="<?php echo $width; ?>">
 		<meta name="twitter:player:height" content="<?php echo $height; ?>">
 		<meta name="twitter:player:title" content="Image">
-		<meta name="twitter:player" content="<?php echo BASE_URL . $f; ?>" />
-	<? }else{ ?>
+		<meta name="twitter:player" content="<?php echo BASE_URL . basename($f); ?>" />
+		<? else: ?>
 		<meta name="twitter:card" content="photo" />
-		<meta name="twitter:image" content="<?php echo BASE_URL . $f; ?>" />
-	<?php } ?>
-	<?php	} ?>
+		<meta name="twitter:image" content="<?php echo BASE_URL . basename($f); ?>" />
+		<?php endif; ?>
+	<?php endif; ?>
 	<?php if(defined('TWITTER_ACCOUNT')): ?>
 		<meta name="twitter:site" content="@<?php echo TWITTER_ACCOUNT; ?>" />
 	<?php endif; ?>
@@ -61,12 +80,3 @@ if (stripos($_SERVER['HTTP_USER_AGENT'], 'Twitterbot') !== false) {
 		Hello, World.
 	</body>
 </html>
-<?php
-	exit;
-}
-
-header('Content-Description: File Transfer');
-header('Content-Type: ' . $mime);
-header('Content-Disposition: ' . ($is_image?'inline':'attachement') . '; filename='.basename($f));
-header('Content-Length: ' . filesize($f));
-readfile($f);
