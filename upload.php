91c6cf3b6c9668950ca0b03597fc180394d2bd0a
PunKeel <punkeel@me.com>
2015-11-27 21:28:01 +0100

    Improve the URL generation process
    Implement my own unique ID generator (with its own custom alphabet)
    
    Fixed: there could be a repeated ID (with different extensions)

--- upload.php
+++ upload.php
@@ -2,22 +2,36 @@
 
 require_once('config.php');
 
-if(empty($_FILES['image'])){
-	echo json_encode(array('success' => 0, 'files' => $_FILES));
+if (empty($_FILES['image'])) {
+	echo json_encode(array(
+			'success' => 0,
+			'files' => $_FILES
+		));
 	exit;
 }
 
 $ext = '.' . substr(strrchr($_FILES['image']['name'], "."), 1);
 
 // Pick up a free name, upload.php is used because it is known to be present
-$uploadfile = 'upload.php';
+$uploadfile = '';
 
-while(file_exists($uploadfile))
-	$uploadfile = substr(sha1(rand(1000, 100000)), 0, rand(5,7));
+function generate_hash($size) {
+	$alphabet = 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXYZ123456789';
+	$s        = '';
+	for ($i = 0; $i < $size; $i++) {
+		$s .= $alphabet[mt_rand(0, strlen($alphabet) - 1)];
+	}
+
+	return $s;
+}
+
+
+while (!empty(glob(BASE_PATH . $uploadfile . '.*')) || empty($uploadfile))
+	$uploadfile = generate_hash(rand(5, 10));
 
 $response = array();
 $response['success'] = move_uploaded_file($_FILES['image']['tmp_name'], BASE_PATH . $uploadfile . $ext);
 
-$response['link'] =  BASE_URL . $uploadfile;
+$response['link'] = BASE_URL . $uploadfile;
 
 echo json_encode($response);
